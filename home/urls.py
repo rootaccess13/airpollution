8dd1d7bf917d11f07485fffbc600d89276a8e8d3
rootaccess13 <43539755+rootaccess13@users.noreply.github.com>
2025-01-12 18:19:29 +0800

    Update urls.py

--- home/urls.py
+++ home/urls.py
@@ -4,3 +4,7 @@ from . views import *
 urlpatterns = [
     path('', home, name="home"),
     path('login/', loginUser, name="login"),
+    path('logout/', logoutUser, name="logout"),
+    path('calibrate/', calibrate, name="calibrate"),
+    path('connection/', connection, name="connection")
+]
